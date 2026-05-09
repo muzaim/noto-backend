6595c0b9f46917959f923264416ec26780566528
Syafri Surya Muzaim <43945260+muzaim@users.noreply.github.com>
2026-05-09 14:51:41 +0700

    push

--- src/common/interceptors/audit-trail.interceptor.ts
+++ src/common/interceptors/audit-trail.interceptor.ts
@@ -97,18 +97,6 @@ export class AuditTrailInterceptor implements NestInterceptor {
             newValues = responseData;
           }
 
-          // await this.auditService.createAuditLog({
-          //   user_id: user?.id,
-          //   username: user?.username,
-          //   table_name: auditMetadata.tableName,
-          //   record_id: recordId,
-          //   action: auditMetadata.action,
-          //   old_values: oldData ? JSON.stringify(oldData) : null,
-          //   new_values: newValues ? JSON.stringify(newValues) : null,
-          //   ip_address: request.ip,
-          //   user_agent: request.get('User-Agent'),
-          //   description: auditMetadata.description,
-          // });
           await this.auditService.createAuditLog({
             user_id: user?.userId,
 
